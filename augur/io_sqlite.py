95088b149d86214d9e5262111ce8f5a7f3a192fe
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-01-20 16:39:44 -0800

    use dict to store read_csv kwargs

--- augur/io_sqlite.py
+++ augur/io_sqlite.py
@@ -6,11 +6,19 @@ DEFAULT_DB_FILE = 'test.sqlite3'
 
 def load_tsv(connection:Connection, tsv_file:str, table_name:str, db_file:str=DEFAULT_DB_FILE, header=True, names=[]):
     connection.execute(f"DROP TABLE IF EXISTS {table_name}")
-    if header:
-        df_chunks = pd.read_csv(tsv_file, sep='\t', chunksize=100000)
-    elif names:
-        df_chunks = pd.read_csv(tsv_file, sep='\t', header=None, names=names, chunksize=100000)
-    else:
+    read_csv_kwargs = {
+        "sep": '\t',
+        "engine": "c",
+        "skipinitialspace": True,
+        "dtype": str,
+        "na_filter": False,
+        "chunksize": 100000,
+    }
+    if not header and not names:
         raise ValueError()
+    if not header and names:
+        read_csv_kwargs['header'] = None
+        read_csv_kwargs['names'] = names
+    df_chunks = pd.read_csv(tsv_file, **read_csv_kwargs)
     for chunk in df_chunks:
         chunk.to_sql(table_name, connection, if_exists='append', index=False)
