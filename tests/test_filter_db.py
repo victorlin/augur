d146087a5296c070086d87636fc90158c7bb9cfa
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-01-31 15:29:50 -0800

    tests: use tmpdir for db file, query using existing filter_obj.cur

--- tests/test_filter_db.py
+++ tests/test_filter_db.py
@@ -1,12 +1,8 @@
 import argparse
 import shlex
 import pytest
-import sqlite3
 import augur.filter
 
-from augur.io_support.db.sqlite import (
-    DEFAULT_DB_FILE
-)
 from augur.filter_support.db.sqlite import (
     METADATA_TABLE_NAME,
     PRIORITIES_TABLE_NAME,
@@ -40,24 +36,14 @@ def tmp_db_file(tmpdir):
     return str(tmpdir / "test.sqlite3")
 
 
-def get_init_filter_obj(args:argparse.Namespace):
+def get_init_filter_obj(args:argparse.Namespace, tmp_db_file):
     filter_obj = FilterSQLite(args)
-    # remove any old db file
-    filter_obj.db_cleanup()
-    # TODO: parameterize db file
-    filter_obj.db_connect()
+    filter_obj.db_connect(tmp_db_file)
     return filter_obj
 
 
-def db_query_fetchall(query:str):
-    con = sqlite3.connect(DEFAULT_DB_FILE)
-    cur = con.cursor()
-    cur.execute(query)
-    return cur.fetchall()
-
-
 class TestFilter:
-    def test_load_metadata(self, tmpdir, argparser):
+    def test_load_metadata(self, tmpdir, argparser, tmp_db_file):
         """Load a metadata file."""
         data = [("strain","location","quality"),
                 ("SEQ_1","colorado","good"),
@@ -65,48 +51,51 @@ class TestFilter:
                 ("SEQ_3","nevada","good")]
         meta_fn = write_metadata(tmpdir, data)
         args = argparser(f'--metadata {meta_fn}')
-        filter_obj = get_init_filter_obj(args)
-        filter_obj.db_load_metadata()
-        table = db_query_fetchall(f"SELECT * FROM {METADATA_TABLE_NAME}")
+        filter_obj = get_init_filter_obj(args, tmp_db_file)
+        filter_obj.db_load_metadata(tmp_db_file)
+        filter_obj.cur.execute(f"SELECT * FROM {METADATA_TABLE_NAME}")
+        table = filter_obj.cur.fetchall()
         assert [row[1:] for row in table] == data[1:]
 
-    def test_load_priority_scores_valid(self, tmpdir, argparser):
+    def test_load_priority_scores_valid(self, tmpdir, argparser, tmp_db_file):
         """Load a priority score file."""
         content = "strain1\t5\nstrain2\t6\nstrain3\t8\n"
         priorities_fn = write_file(tmpdir, "priorities.txt", content)
         # --metadata is required but we don't need it
         args = argparser(f'--metadata "" --priority {priorities_fn}')
-        filter_obj = get_init_filter_obj(args)
-        filter_obj.db_load_priorities_table()
-        table = db_query_fetchall(f"SELECT * FROM {PRIORITIES_TABLE_NAME}")
+        filter_obj = get_init_filter_obj(args, tmp_db_file)
+        filter_obj.db_load_priorities_table(tmp_db_file)
+        filter_obj.cur.execute(f"SELECT * FROM {PRIORITIES_TABLE_NAME}")
+        table = filter_obj.cur.fetchall()
         assert table == [(0, 'strain1', 5.0), (1, 'strain2', 6.0), (2, 'strain3', 8.0)]
 
-    def test_load_priority_scores_malformed(self, tmpdir, argparser):
+    def test_load_priority_scores_malformed(self, tmpdir, argparser, tmp_db_file):
         """Attempt to load a priority score file with non-float in priority column raises a ValueError."""
         content = "strain1 X\n"
         priorities_fn = write_file(tmpdir, "priorities.txt", content)
         # --metadata is required but we don't need it
         args = argparser(f'--metadata "" --priority {priorities_fn}')
-        filter_obj = get_init_filter_obj(args)
+        filter_obj = get_init_filter_obj(args, tmp_db_file)
         with pytest.raises(ValueError) as e_info:
-            filter_obj.db_load_priorities_table()
+            filter_obj.db_load_priorities_table(tmp_db_file)
         assert str(e_info.value) == "Failed to parse priority file."
 
-    def test_load_priority_scores_valid_with_spaces_and_tabs(self, tmpdir, argparser):
+    def test_load_priority_scores_valid_with_spaces_and_tabs(self, tmpdir, argparser, tmp_db_file):
         """Load a priority score file with spaces in strain names."""
         content = "strain 1\t5\nstrain 2\t6\nstrain 3\t8\n"
         priorities_fn = write_file(tmpdir, "priorities.txt", content)
         # --metadata is required but we don't need it
         args = argparser(f'--metadata "" --priority {priorities_fn}')
-        filter_obj = get_init_filter_obj(args)
-        filter_obj.db_load_priorities_table()
-        table = db_query_fetchall(f"SELECT * FROM {PRIORITIES_TABLE_NAME}")
+        filter_obj = get_init_filter_obj(args, tmp_db_file)
+        filter_obj.db_load_priorities_table(tmp_db_file)
+        filter_obj.cur.execute(f"SELECT * FROM {PRIORITIES_TABLE_NAME}")
+        table = filter_obj.cur.fetchall()
         assert table == [(0, 'strain 1', 5.0), (1, 'strain 2', 6.0), (2, 'strain 3', 8.0)]
 
-    def test_load_priority_scores_does_not_exist(self, tmpdir, argparser):
+    def test_load_priority_scores_does_not_exist(self, tmpdir, argparser, tmp_db_file):
         """Attempt to load a non-existant priority score file raises a FileNotFoundError."""
         invalid_priorities_fn = str(tmpdir / "does/not/exist.txt")
         args = argparser(f'--metadata "" --priority {invalid_priorities_fn}')
-        filter_obj = get_init_filter_obj(args)
+        filter_obj = get_init_filter_obj(args, tmp_db_file)
         with pytest.raises(FileNotFoundError):
-            filter_obj.db_load_priorities_table()
+            filter_obj.db_load_priorities_table(tmp_db_file)
