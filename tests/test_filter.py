372e6636e99aa336403113ffb4e31bb4f6796060
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-01-31 14:46:01 -0800

    remove translated priority loading tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -41,28 +41,11 @@ def write_metadata(tmpdir, metadata):
         fh.write("\n".join(("\t".join(md) for md in metadata)))
     return fn
 
-@pytest.fixture
-def mock_priorities_file_valid(mocker):
-    mocker.patch(
-        "builtins.open", mocker.mock_open(read_data="strain1 5\nstrain2 6\nstrain3 8\n")
-    )
-
-
-@pytest.fixture
-def mock_priorities_file_malformed(mocker):
-    mocker.patch("builtins.open", mocker.mock_open(read_data="strain1 X\n"))
-
 
 @pytest.fixture
 def mock_run_shell_command(mocker):
     mocker.patch("augur.filter.run_shell_command")
 
-
-@pytest.fixture
-def mock_priorities_file_valid_with_spaces_and_tabs(mocker):
-    mocker.patch(
-        "builtins.open", mocker.mock_open(read_data="strain 1\t5\nstrain 2\t6\nstrain 3\t8\n")
-    )
 
 class TestFilter:
     def test_read_vcf_compressed(self):
@@ -80,29 +63,6 @@ class TestFilter:
         assert len(seq_keep) == 150
         assert seq_keep[149] == "G22733"
         assert seq_keep == all_seq
-
-    def test_read_priority_scores_valid(self, mock_priorities_file_valid):
-        # builtins.open is stubbed, but we need a valid file to satisfy the existence check
-        priorities = augur.filter.read_priority_scores(
-            "tests/builds/tb/data/lee_2015.vcf"
-        )
-
-        assert priorities == {"strain1": 5, "strain2": 6, "strain3": 8}
-        assert priorities["strain1"] == 5
-        assert priorities["strain42"] == 0, "Default priority is 0 for unlisted sequences"
-
-    def test_read_priority_scores_malformed(self, mock_priorities_file_malformed):
-        with pytest.raises(ValueError):
-            # builtins.open is stubbed, but we need a valid file to satisfy the existence check
-            augur.filter.read_priority_scores("tests/builds/tb/data/lee_2015.vcf")
-
-    def test_read_priority_scores_valid_with_spaces_and_tabs(self, mock_priorities_file_valid_with_spaces_and_tabs):
-        # builtins.open is stubbed, but we need a valid file to satisfy the existence check
-        priorities = augur.filter.read_priority_scores(
-            "tests/builds/tb/data/lee_2015.vcf"
-        )
-
-        assert priorities == {"strain 1": 5, "strain 2": 6, "strain 3": 8}
 
     def test_read_priority_scores_does_not_exist(self):
         with pytest.raises(FileNotFoundError):
