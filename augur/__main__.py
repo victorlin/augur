376f5cc0bb18a0386bdd7f30a4bd12f9e3d175ba
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-10-31 09:58:43 -0700

    🚧 Enable cProfile at entry point

--- augur/__main__.py
+++ augur/__main__.py
@@ -5,6 +5,7 @@ Stub function and module used as a setuptools entry point.
 import sys
 import augur
 from sys import argv, exit
+import cProfile
 
 # Entry point for setuptools-installed script and bin/augur dev wrapper.
 def main():
@@ -21,7 +22,14 @@ def main():
         newline=None,
     )
 
-    return augur.run( argv[1:] )
+    profiler = cProfile.Profile()
+    profiler.enable()
+    try:
+        result = augur.run( argv[1:] )
+    finally:
+        profiler.disable()
+        profiler.dump_stats("stats.prof") # Creates file in working directory. Visualize with snakeviz or https://nejc.saje.info/pstats-viewer.html
+    return result
 
 # Run when called as `python -m augur`, here for good measure.
 if __name__ == "__main__":
